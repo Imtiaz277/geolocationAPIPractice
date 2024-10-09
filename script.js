d82e5f481ca6521b2d219248d6059044df3c5851
Imtiaz277 <103617542+Imtiaz277@users.noreply.github.com>
2024-10-09 11:52:31 +0800

    update script.js

--- script.js
+++ script.js
@@ -20,9 +20,11 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+const copyrightText = document.querySelector(".copyright");
 
 function success(position) {
   console.log(position);
+  copyrightText.text = position.latitude + " " + position.longitude;
 }
 
 function fail() {
