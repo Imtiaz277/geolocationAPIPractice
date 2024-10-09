9b363aa61359bbef1694c6bbce8f5fc84b2fe5a7
Imtiaz277 <103617542+Imtiaz277@users.noreply.github.com>
2024-10-09 11:41:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+function success(position) {
+  console.log(position);
+}
+
+function fail() {
+  alert("Could not get your position.");
+}
+
+// Check if navigator exists (older browsers don't support navigator)
+if (navigator.geolocation) {
+  // Get user's current location
+  // 1st callback function is when the browser successfully grabbed the user's current location
+  // 2nd callback function is when it fails
+  // 3rd argument: To get more accurate current location
+  navigator.geolocation.getCurrentPosition(success, fail);
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
